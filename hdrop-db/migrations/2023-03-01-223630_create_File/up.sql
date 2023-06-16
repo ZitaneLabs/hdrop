97802c83c692abf4a206772d1f4873da71a4997b
SplittyDev <splittydev@gmail.com>
2023-06-16 17:45:06 +0200

    Refactor diesel DSL stuff

--- hdrop-db/migrations/2023-03-01-223630_create_File/up.sql
+++ hdrop-db/migrations/2023-03-01-223630_create_File/up.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS "files" (
-            "uuid" UUID NOT NULL,
-            "accessToken" TEXT NOT NULL,
-            "updateToken" TEXT NOT NULL,
-            "dataUrl" TEXT,
-            "fileNameData" TEXT NOT NULL,
-            "fileNameHash" TEXT NOT NULL,
-            "salt" TEXT NOT NULL,
-            "iv" TEXT NOT NULL,
-            "createdAt" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "expiresAt" timestamp with time zone NOT NULL,
-        
-            CONSTRAINT "files_pkey" PRIMARY KEY ("uuid")
-        );
+    "uuid" UUID NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "updateToken" TEXT NOT NULL,
+    "dataUrl" TEXT,
+    "fileNameData" TEXT NOT NULL,
+    "fileNameHash" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "iv" TEXT NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" timestamp with time zone NOT NULL,
 
-        CREATE UNIQUE INDEX "files_uuid_key" ON "files"("uuid");
-        CREATE UNIQUE INDEX "files_accessToken_key" ON "files"("accessToken");
+    CONSTRAINT "files_pkey" PRIMARY KEY ("uuid")
+);
+
+CREATE UNIQUE INDEX "files_uuid_key" ON "files"("uuid");
+CREATE UNIQUE INDEX "files_accessToken_key" ON "files"("accessToken");
--- hdrop-db/src/database.rs
+++ hdrop-db/src/database.rs
@@ -1,12 +1,16 @@
-use crate::{error::Result, models::*, schema::files::dsl::*, utils};
-use ::uuid::Uuid;
 use chrono::Utc;
 use deadpool_diesel::postgres::{Manager, Pool};
 use diesel::prelude::*;
 use hdrop_shared::responses;
-use std::borrow::Cow;
-use std::env;
-use utils::{TokenGenerator, UPDATE_TOKEN_LENGTH};
+use std::{borrow::Cow, env};
+use uuid::Uuid;
+
+use crate::{
+    error::Result,
+    models::*,
+    schema::files::dsl as files_table,
+    utils::{TokenGenerator, UPDATE_TOKEN_LENGTH},
+};
 
 pub struct Database {
     pool: Pool,
@@ -14,10 +18,10 @@ pub struct Database {
 }
 
 impl Database {
-    /// Tries to create Database from environment variables. Panics if there is no env var.
+    /// Initialize the database from environment variables.
     pub fn try_from_env() -> Result<Database> {
         // DATABASE_URL must be set, therefore panic is reasonable here.
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+        let database_url = env::var("DATABASE_URL")?;
         let manager = Manager::new(database_url, deadpool_diesel::Runtime::Tokio1);
         let pool = Pool::builder(manager).max_size(8).build()?;
         let generator = TokenGenerator::default();
@@ -30,7 +34,7 @@ impl Database {
             .get()
             .await?
             .interact(|conn| {
-                diesel::insert_into(files)
+                diesel::insert_into(files_table::files)
                     .values(file)
                     .get_result::<File>(conn)
             })
@@ -43,7 +47,7 @@ impl Database {
             .get()
             .await?
             .interact(|conn| {
-                diesel::update(files.filter(uuid.eq(file.uuid)))
+                diesel::update(files_table::files.filter(files_table::uuid.eq(file.uuid)))
                     .set(file)
                     .execute(conn)
                     .map(|_| ())
@@ -53,7 +57,7 @@ impl Database {
 
     pub async fn update_data_url<'a>(
         &self,
-        s_uuid: Uuid,
+        uuid: Uuid,
         file_url: Option<impl Into<Cow<'a, str>>>,
     ) -> Result<()> {
         let file_url: Option<String> = file_url.map(|inner| inner.into().into_owned());
@@ -62,8 +66,8 @@ impl Database {
             .get()
             .await?
             .interact(move |conn| {
-                diesel::update(files.filter(uuid.eq(s_uuid)))
-                    .set(dataUrl.eq(file_url))
+                diesel::update(files_table::files.filter(files_table::uuid.eq(uuid)))
+                    .set(files_table::dataUrl.eq(file_url))
                     .execute(conn)
                     .map(|_| ())
             })
@@ -76,20 +80,24 @@ impl Database {
             .get()
             .await?
             .interact(move |conn| {
-                diesel::update(files.filter(uuid.eq(file.uuid)))
-                    .set(expiresAt.eq(file.expiresAt))
+                diesel::update(files_table::files.filter(files_table::uuid.eq(file.uuid)))
+                    .set(files_table::expiresAt.eq(file.expiresAt))
                     .execute(conn)
                     .map(|_| ())
             })
             .await??)
     }
 
-    pub async fn get_file_by_uuid(&self, s_uuid: Uuid) -> Result<File> {
+    pub async fn get_file_by_uuid(&self, uuid: Uuid) -> Result<File> {
         Ok(self
             .pool
             .get()
             .await?
-            .interact(move |conn| files.filter(uuid.eq(s_uuid)).first(conn))
+            .interact(move |conn| {
+                files_table::files
+                    .filter(files_table::uuid.eq(uuid))
+                    .first(conn)
+            })
             .await??)
     }
 
@@ -102,7 +110,11 @@ impl Database {
             .pool
             .get()
             .await?
-            .interact(move |conn| files.filter(accessToken.eq(access_token)).first(conn))
+            .interact(move |conn| {
+                files_table::files
+                    .filter(files_table::accessToken.eq(access_token))
+                    .first(conn)
+            })
             .await??)
     }
 
@@ -137,9 +149,9 @@ impl Database {
             .get()
             .await?
             .interact(move |conn| {
-                files
-                    .filter(expiresAt.lt(Utc::now()))
-                    .select(uuid)
+                files_table::files
+                    .filter(files_table::expiresAt.lt(Utc::now()))
+                    .select(files_table::uuid)
                     .load::<Uuid>(conn)
             })
             .await??)
@@ -164,18 +176,19 @@ impl Database {
             .get()
             .await?
             .interact(move |conn| {
-                diesel::delete(files.filter(uuid.eq(&file.uuid))).get_result(conn)
+                diesel::delete(files_table::files.filter(files_table::uuid.eq(&file.uuid)))
+                    .get_result(conn)
             })
             .await??)
     }
 
-    pub async fn delete_file_by_uuid(&self, s_uuid: Uuid) -> Result<()> {
+    pub async fn delete_file_by_uuid(&self, uuid: Uuid) -> Result<()> {
         Ok(self
             .pool
             .get()
             .await?
             .interact(move |conn| {
-                diesel::delete(files.filter(uuid.eq(s_uuid)))
+                diesel::delete(files_table::files.filter(files_table::uuid.eq(uuid)))
                     .execute(conn)
                     .map(|_| ())
             })
@@ -192,8 +205,8 @@ impl Database {
             .get()
             .await?
             .interact(|conn| {
-                files
-                    .filter(accessToken.eq(access_token))
+                files_table::files
+                    .filter(files_table::accessToken.eq(access_token))
                     .first::<File>(conn)
                     .is_ok()
             })

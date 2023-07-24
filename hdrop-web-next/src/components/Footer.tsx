e62c9d0f1547d6cd6cb27081f84dead4dfc86053
SplittyDev <splittydev@protonmail.com>
2023-07-25 00:49:47 +0200

    web: Add GitHub link to footer

--- hdrop-web-next/src/components/Footer.tsx
+++ hdrop-web-next/src/components/Footer.tsx
@@ -3,17 +3,22 @@ import Wave from "react-wavify"
 export default function Footer() {
     return (
         <footer>
-            <div className="w-full h-16">
-                <Wave
-                    fill='hsl(0,0%,25%)'
-                    paused={false}
-                    options={{
-                        height: 1,
-                        amplitude: 10,
-                        speed: 0.25,
-                        points: 3,
-                    }}
-                />
+            <div className="relative w-full h-16">
+                <div className="absolute top-0 bottom-0 left-0 w-full flex justify-center z-50 pt-7 text-sm">
+                    <a className="text-gray-300 hover:text-gray-100 cursor-pointer" href="https://github.com/ZitaneLabs/hdrop" target="_blank" rel="noopener noreferrer">Fork hdrop on GitHub</a>
+                </div>
+                <div className="absolute top-0 bottom-0 left-0 w-full z-0">
+                    <Wave
+                        fill='hsl(0,0%,25%)'
+                        paused={false}
+                        options={{
+                            height: 1,
+                            amplitude: 10,
+                            speed: 0.25,
+                            points: 3,
+                        }}
+                    />
+                </div>
             </div>
         </footer>
     )

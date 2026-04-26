1f5460dc00fa37d1b0cb0a68220e0370da7f09d2
SplittyDev <splittydev@protonmail.com>
2026-04-26 08:15:38 +0700

    Harden containers and restrict capabilities
    Further harden api Dockerfile
    
    Create unprivileged user for api docker image
    
    Exclude keys and secrets from docker image builds
    
    Spawn services through init process and limit pids
    
    Setup log rotation and limiting for all services

--- hdrop-server/infra/entrypoint.sh
+++ hdrop-server/infra/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
+set -eu
 
 # Migrate database
 diesel migration run --migration-dir ./hdrop-db/migrations
 
 # Start server
-pm2-runtime start /usr/local/bin/hdrop-server --name hdrop-server
+exec /usr/local/bin/hdrop-server
